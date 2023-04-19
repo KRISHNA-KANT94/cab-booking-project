7c49a4718be5ecd73d29ae2e7f2ce255cc5f2df4
dac <krishnakant94@gmail.com>
2023-04-19 16:33:17 +0530

    commiting

---
+++ userinfo.sql
@@ -0,0 +1,4 @@
+mysql> create table
+    -> userinfo
+    -> (userid int primary key, username varchar(20), contact varchar(20), email varchar(20));
+mysql> insert into userinfo values (100, 'abc','898988998', 'sabs@gmail.com'), (101, 'dfgsd', '797686698','dffafa@ds.com');
